ed16955ea24ca180828dbb4f978738075e50787e
Andrew Betts <abetts@ucsd.edu>
2014-09-28 08:11:04 -0700

    Integer Lister

--- practice.c
+++ practice.c
@@ -4,11 +4,25 @@
 
 int main(void)
 {
+char [] z0 = "Zero";
+char [] o0 = "One";
+char [] t0 = "Two";
+char [] th0 = "Three";
+char [] f0 = "Four";
+char [] fi0 = "Five";
+char [] s0 = "Six";
+char [] se0 = "Seven";
+char [] e0 = "Eight";
+char [] n0 = "Nine";
+double x,y;
+scanf("%d",&x);
 
-
-
-
-
-
+if( x<0 ) printf("%c",'-');
+y=i>=1000000000?10:i>=100000000?9:i>=10000000?8:i>=1000000?7:i>=100000?6:i>=10000?5:i>=1000?4:i>=100?3:i>=10?2:1;
+for(int i=0;i<y;++i){
+printf("%c",x%(pow(10,i+1);
 
 }
+
+}
+i
