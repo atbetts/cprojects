ca3f809bbac4a97fd925b53db90edd32ee9f728c
Andrew Betts <abetts@ucsd.edu>
2014-10-07 16:27:17 -0700

    Updated stuff

--- test.c
+++ test.c
@@ -1,6 +1,79 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define SIZEARR(x) (sizeof(x)/sizeof(x[0]))
+int* selectionSort(int* arr);
+
+
 int main (){
+
+int test[10] = {1,2,3,4,5,3,3,1,0,2};
+
+selectionSort(test);
+
+return 0;
+
+}
+
+int printDisplay(int * arr){
+
+/* 
+Prints a graphical display of an array
+of ints as lines of asteriks on each new line 
+*/
+
+for(int j =0; j<SIZEARR(arr) ; j++){
+for(int i =0; i<arr[j]; i++)
+printf("*");
+printf("\n");
+}
+
+
+return 1;
+}
+
+int swap (int a, int b, int * arr){
+
+/*Swaps elements a and b in array arr*/
+
+int temp = arr[a];
+arr[a]=arr[b];
+arr[b]=temp;
+
+return 1;
+}
+
+int* selectionSort (int * arr){
+/*
+Sorts a list of ints in descending order
+by swapping the first element in each
+increasingly smaller list with the max of
+said list
+*/
+
+int size = (int)SIZEARR(arr);
+
+ int max,i,j;
+ for(i =0;i<size;i++){
+ max = i;
+ for(j=i+1;j<size;j++){
+	 if(arr[j]>arr[max]){
+	 max = j;
+	 }
+ }
+ swap(i,max,arr);
+
+ printDisplay(arr);
+
+ printf("Press Enter to continue.\n");
+
+ getchar();
+
+ }
+
+return arr;
+}
+
+int triangleDot(int row){
  /*Triangle Dots*/
  int x;
  int y;
@@ -19,26 +92,7 @@ int main (){
  }
   
  return EXIT_SUCCESS;
-} 
-
-int commitGit(){
-/*Testing GITHUB commit*/
-
-return 1;
 }
-
-int triangleDot(int row){
-return (row*(row+1))/2;
-}
-
-int numreverse(int u){
- if (u<10) return u;
- 
-
-
-return 5;
-}
-
 int lengthOfInt(unsigned i){
 int x;
 x=i>=1000000000?10:i>=100000000?9:i>=10000000?8:i>=1000000?7:i>=100000?6:i>=10000?5:i>=1000?4:i>=100?3:i>=10?2:1;
